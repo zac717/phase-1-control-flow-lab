08692965900c44a2dccdd59935816c81847323e9
isaac <isaacongera717@gmail.com>
2022-11-26 10:19:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400)
+  {
+    return "This one is on me!"
+  }
+    else if (someValue >2000 && someValue <2500)
+     {
+      return "I will gladly take your thirty bucks."
+    }
+    else if (someValue >2500)
+    {
+      return "No can do."
+    }
+  }
+
+
+function ternaryCheckCity(City){
+  // Write your code here!
+  
+  if(City=== "NYC")
+  {
+    return "Ok, sounds good."
+  }
+  else
+  {
+    return ("No go.")
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if(tip==="generous" ){
+    return "Thank you so much."
+  }
+  else if (tip === "not as generous"){
+  return "Thank you."
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+else{
+  return "Bye."
+}
 }
